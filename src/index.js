ad535648a8303d9f90563a7599330ac546fc9790
noudewato <noudewatofawaz7@gmail.com>
2024-03-28 11:37:35 +0000

    added a new config to avoid page not found on netlify

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import "react-image-gallery/styles/css/image-gallery.css";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 import { Provider } from "react-redux";
+import "react-image-crop/dist/ReactCrop.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
